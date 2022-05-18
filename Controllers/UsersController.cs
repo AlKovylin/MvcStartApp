d192b04f86d6126c120553c1f6919f01801985ca
Ковылин Алексей <alkovylin@ya.ru>
2022-05-18 17:05:25 +0300

    HomeController

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcStartApp.Models.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IBlogRepository _repo;
+
+        public UsersController(IBlogRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _repo.GetUsers();
+            return View(authors);
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
